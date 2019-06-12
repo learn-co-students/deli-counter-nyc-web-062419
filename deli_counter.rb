6112f224cf10ceaffc35fe73baafcb1884fe20f4
github username <github email address>
2019-06-12 13:26:53 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each.with_index(1) do |customer, i|
-      current_line << " #{i}. #{customer}"
+    katz_deli.each.with_index(1) do |name, i|
+      current_line << " #{i}. #{name}"
     end
     puts current_line
   end
